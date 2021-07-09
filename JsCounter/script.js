4b62fa1c4fe2052ef8b624d607f2b87bfbf8f78d
Collins-Nonso <61317981+Collins-Nonso@users.noreply.github.com>
2021-07-09 23:21:06 +0100

    Add files via upload

---
+++ JsCounter/script.js
@@ -0,0 +1,18 @@
+
+document.getElementById('goUp').addEventListener('click',function myFunction(){ 
+    var value = parseInt(document.getElementById('value').innerHTML) 
+    if(value >= 50){ 
+        var newValue = value;
+        document.getElementById('value').innerHTML = newValue }
+        else{ 
+            var newValue = value + 1;
+            document.getElementById('value').innerHTML = newValue }}) 
+            
+document.getElementById('goDown').addEventListener('click',function myFunction(){ 
+    var value = parseInt(document.getElementById('value').innerHTML) 
+    if(value <= 0){ 
+        var newValue = value;
+        document.getElementById('value').innerHtml = newValue }
+        else{ 
+            var newValue = value - 1;
+            document.getElementById('value').innerHTML = newValue }})
